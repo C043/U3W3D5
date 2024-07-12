afa2d4fe1cb6162e265d30574ff86f66fffe6664
C043 <mariofragnitoph@gmail.com>
2024-07-12 16:22:08 +0200

    Adds like function

--- src/components/SpotifyPlayer.jsx
+++ src/components/SpotifyPlayer.jsx
@@ -1,19 +1,47 @@
+import { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import { Heart, HeartFill } from "react-bootstrap-icons";
+import { useDispatch, useSelector } from "react-redux";
+import { dislikeAction, likeAction } from "../redux/actions";
 
 const SpotifyPlayer = () => {
+  const [liked, setLiked] = useState(false);
+  const dispatch = useDispatch();
+
   const selectedSong = useSelector(state => state.selected.content);
+  const likedList = useSelector(state => state.liked.content);
+  useEffect(() => {
+    if (likedList.filter(s => s.id === selectedSong.id).length >= 1) {
+      setLiked(true);
+    } else {
+      setLiked(false);
+    }
+  }, [likedList, selectedSong]);
+
   return (
     <Container fluid className="fixed-bottom bg-container pt-1">
       <Row className="h-100">
         {selectedSong && (
           <Col xs={{ span: 2, offset: 2 }} className="position-absolute mt-2">
-            <div className="d-flex gap-2">
+            <div className="d-flex gap-2 align-items-center">
               <img src={selectedSong.album.cover_small} alt="selected-song-cover" />
               <div className="d-flex flex-column text-light">
                 <p className="m-0 text-truncate">{selectedSong.title}</p>
                 <p className="m-0 text-truncate">{selectedSong.artist.name}</p>
               </div>
+              {liked ? (
+                <HeartFill
+                  type="button"
+                  onClick={() => dispatch(dislikeAction(selectedSong))}
+                  className={"d-block text-light flex-shrink-0"}
+                />
+              ) : (
+                <Heart
+                  type="button"
+                  onClick={() => dispatch(likeAction(selectedSong))}
+                  className={"d-block text-light flex-shrink-0"}
+                />
+              )}
             </div>
           </Col>
         )}
--- src/redux/reducers/likedReducer.js
+++ src/redux/reducers/likedReducer.js
@@ -1,9 +1,21 @@
+import { DISLIKE, LIKE } from "../actions";
+
 const initialState = {
   content: [],
 };
 
 const likedReducer = (state = initialState, action) => {
   switch (action.type) {
+    case LIKE:
+      return {
+        ...state,
+        content: [...state.content, action.payload],
+      };
+    case DISLIKE:
+      return {
+        ...state,
+        content: state.content.filter(song => song.id !== action.payload.id),
+      };
     default:
       return state;
   }
